67d2de1b526c1423274f18624975641491f3a806
ewelinacupak-collab <ewelinacupak@gmail.com>
2025-11-15 11:24:44 +0100

    Add files via upload

---
+++ Zad.9.py
@@ -0,0 +1,30 @@
+imie = input("Podaj imie: ")
+
+print("Witaj", imie)
+
+wiek = int(input("Podaj wiek: "))
+
+print("Twój wiek to: ", wiek)
+
+nazwisko = input("Podaj nazwisko: ")
+
+print(imie[0], nazwisko[0])
+
+x = input("Podaj tekst: ")
+
+for i in range(5):
+    print(x)
+
+z = input("Podaj 2 tekst: ")
+
+trzeci = x + z
+
+print(trzeci)
+
+czwarty = imie + nazwisko
+
+dlugosc = len(czwarty)
+
+polowa = round((dlugosc / 2),0)
+
+print(czwrty[0,polowa])
