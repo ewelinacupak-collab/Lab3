a0969187e984794ca44a219947d05074cf36bdc0
ewelinacupak-collab <ewelinacupak@gmail.com>
2025-11-21 20:34:09 +0100

    Update Zad.8.py

--- Zad.8.py
+++ Zad.8.py
@@ -6,10 +6,11 @@ print("Znak o indeksie zerowym:", Tekst2)
 Tekst3 = Tekst[-1]
 print("Znak o indeksie ostatnim:", Tekst3)
 Tekst4 = Tekst[0::2]
-print("Znaki co drugi zaczynając od zerowego", Tekst4)
+print("Znaki co drugi zaczynając od zerowego:", Tekst4)
 Tekst5 = Tekst[1::3]
-print("Znaki co trzeci zaczynając od pierwszego", Tekst5)
+print("Znaki co trzeci zaczynając od pierwszego:", Tekst5)
 Tekst6 = Tekst[10::1]
-print("Znaki od dziesiątego do ostatniego", Tekst6)
+print("Znaki od dziesiątego do ostatniego:", Tekst6)
 Tekst7 = Tekst[::-1]
-print("Znaki od końca do początku", Tekst7)
+
+print("Znaki od końca do początku:", Tekst7)
